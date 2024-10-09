a17a4c12fc9518f54a17d875d79b3a470c2a64ca
pinis-gini-apps <pinis@gini-apps.com>
2024-10-09 10:15:14 +0300

    update workflow

--- scripts/ci-cd-scripts/pathScript.js
+++ scripts/ci-cd-scripts/pathScript.js
@@ -4,10 +4,8 @@ console.log(process.env.TEST_TAG)
 const { execSync } = require('child_process')
 
 const testTag = process.env.TEST_TAG
-console.log(testTag)
-// const cucumberCommand = `./node_modules/.bin/cucumber-js --require-module @babel/register --require-module @babel/polyfill -f json:tests/reports/cucumber_report.json -f html:tests/reports/cucumber_report_default.html tests -t '${testTag}'`
-const cucumberCommand =
-  './node_modules/.bin/cucumber-js --require-module @babel/register --require-module @babel/polyfill -f json:tests/reports/cucumber_report.json -f html:tests/reports/cucumber_report_default.html tests -t @smoke1 @smoke2'
+const cucumberCommand = `./node_modules/.bin/cucumber-js --require-module @babel/register --require-module @babel/polyfill -f json:tests/reports/cucumber_report.json -f html:tests/reports/cucumber_report_default.html tests -t '${testTag}'`
+
 try {
   execSync(cucumberCommand, { stdio: 'inherit' })
 } catch (err) {
