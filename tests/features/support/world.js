f5b5a0f604983cb2af1400896267698dd7254ad1
Olena Zhelnytska <olena.zhelnytska@globallogic.com>
2025-06-22 19:49:15 +0200

    updated zoom to 0.7

--- tests/features/support/world.js
+++ tests/features/support/world.js
@@ -72,7 +72,7 @@ class CustomWorld extends World {
       .build()
   }
 
-  async zoomOut(factor = 0.9) {
+  async zoomOut(factor = 0.7) {
     await this.driver.executeScript(`document.body.style.zoom = '${factor}'`)
   }
 }
