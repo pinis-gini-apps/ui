68cc4c18783f2fdc04512f995372e76eacc90add
Olena Zhelnytska <olena.zhelnytska@globallogic.com>
2025-06-22 20:07:29 +0200

    fixed zoom to 1.0

--- tests/features/support/world.js
+++ tests/features/support/world.js
@@ -51,8 +51,8 @@ class CustomWorld extends World {
         browseConfigs.addArguments('no-sandbox')
         browseConfigs.addArguments('disable-gpu')
         browseConfigs.addArguments(`window-size=${screen_size.width},${screen_size.height}`)
-        browseConfigs.addArguments('force-device-scale-factor=0.9') // Optional
-        browseConfigs.addArguments('high-dpi-support=1') // Optional
+        // browseConfigs.addArguments('force-device-scale-factor=0.9') // Optional
+        // browseConfigs.addArguments('high-dpi-support=1') // Optional
       }
     }
 
